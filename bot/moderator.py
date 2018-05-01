90fffd7f3f643d800b16191fffd9925b0ada6ec1
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-01 20:19:21 +1000

    Update moderator.py

--- bot/moderator.py
+++ bot/moderator.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands
+import logging
+import asyncio
+import random
+import time
+import os
 
+client = commands.Bot(command_prefix="Mike")
+footer_text = "Open Mic"
+
+@client.event
+async def on_ready():
+    print("Logged in as:")
+    print(client.user.name)
+    print(client.user.id)
+    print("---------------")
+    await client.change_presence(game=discord.Game(name='wit Huskie'))
+    
+client.run(os.environ['BOT_TOKEN'])
