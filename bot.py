6dbbb2a8ad48c9e06f837a931616bd52330e8401
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-02 08:23:15 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -365,4 +365,37 @@ async def help(ctx):
     print("}help")
     print("{} ### {}".format(author, author.id))
     print("============================================================")
+    
+# }userinfo <user>
+@client.command(pass_context=True)
+async def userinfo(ctx, userName: discord.Member = None):
+    member_role = discord.utils.get(ctx.message.server.roles, name='Listener')
+    staff_role = discord.utils.get(ctx.message.server.roles, name='Staff')
+    author = ctx.message.author
+    msg = discord.Embed(colour=0xdb5000, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if member_role in author.roles or staff_role in author.roles:
+        if userName == None:
+            msg.title = ""
+            msg.add_field(name="       :warning: ", value="`Mike userinfo (user)`")
+        else:
+            imageurl = userName.avatar_url
+            msg.title = ":closed_book: USER INFORMATION"
+            msg.set_thumbnail(url=imageurl)
+            msg.add_field(name="NAME:", value=(userName), inline=True)
+            msg.add_field(name="ID:", value=(userName.id), inline=True)
+            msg.add_field(name="CREATED AT:", value=(userName.created_at), inline=True)
+            msg.add_field(name="JOINED AT:", value=(userName.joined_at), inline=True)
+            msg.add_field(name="STATUS:", value=(userName.status), inline=True)
+            msg.add_field(name="IS BOT:", value=(userName.bot), inline=True)
+            msg.add_field(name="GAME:", value="Playing {}".format(userName.game), inline=True)
+            msg.add_field(name="NICKNAME:", value=(userName.nick), inline=True)
+            msg.add_field(name="TOP ROLE:", value=(userName.top_role), inline=True)
+            msg.add_field(name="VOICE CHANNEL:", value=(userName.voice_channel), inline=True)
+    await client.say(embed=msg)
+    print("============================================================")
+    print("<userinfo <user>")
+    print("{} ### {}".format(author, author.id))
+    print("============================================================")
 client.run(os.environ['BOT_TOKEN'])
