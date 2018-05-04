d0bf932f6455cbb7994f2e40090eb936966cb743
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-04 11:38:18 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -350,21 +350,6 @@ licklinks = ["https://i.imgur.com/QkRz1GJ.gif",
              "https://i.imgur.com/4QIlJtC.gif",
              "https://i.imgur.com/LptJIi1.gif",
              "https://i.imgur.com/THGgRJz.gif"]
-
-# }help
-client.remove_command('help')
-@client.command(pass_context=True)
-async def help(ctx):
-    author = ctx.message.author
-    msg = discord.Embed(colour=0xdb5000, description= "")
-    msg.title = ""
-    msg.add_field(name=":incoming_envelope: ", value="`You can see all commands in the #mike-commands channel!`")
-    msg.set_footer(text=footer_text)
-    await client.say(embed=msg)
-    print("============================================================")
-    print("}help")
-    print("{} ### {}".format(author, author.id))
-    print("============================================================")
     
 # }userinfo <user>
 @client.command(pass_context=True)
